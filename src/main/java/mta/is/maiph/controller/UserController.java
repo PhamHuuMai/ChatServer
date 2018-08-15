24c45ab4f3852faaa10a464a527d8be264043ad9
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-08-15 07:20:53 +0700

    no message

--- src/main/java/mta/is/maiph/controller/UserController.java
+++ src/main/java/mta/is/maiph/controller/UserController.java
@@ -5,7 +5,6 @@
  */
 package mta.is.maiph.controller;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.util.LinkedList;
 import java.util.List;
 import mta.is.maiph.constant.ErrorCode;
@@ -26,7 +25,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
