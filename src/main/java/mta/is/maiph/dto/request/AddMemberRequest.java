063ab11377020feead54a81ecb1971ac1721eced
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-09-05 00:39:52 +0700

    cc

---
+++ src/main/java/mta/is/maiph/dto/request/AddMemberRequest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mta.is.maiph.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author MaiPH
+ */
+@Setter
+@Getter
+@ToString
+public class AddMemberRequest {
+    String memberId;
+    String cvsId;
+}
