bf85aa178679948ca1dfbf30b8d72e97c6a84cc0
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-09-09 19:40:41 +0700

    xxx

--- src/main/java/mta/is/maiph/exception/ExceptionHandle.java
+++ src/main/java/mta/is/maiph/exception/ExceptionHandle.java
@@ -1,5 +1,6 @@
 package mta.is.maiph.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import mta.is.maiph.constant.ErrorCode;
 import mta.is.maiph.dto.response.Response;
 import org.springframework.http.HttpStatus;
@@ -11,17 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  *
  * @author MaiPH
  */
+@Slf4j
 @RestControllerAdvice()
 public class ExceptionHandle {
     
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity app(ApplicationException ex){ 
+        log.info(""+ex.getMessage());
         Response response = new Response(ex.getCode());
         return new ResponseEntity(response,HttpStatus.OK);
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity common(Exception ex) { 
-        ex.printStackTrace();
+        log.info(ex.getMessage());
         Response response = new Response(ErrorCode.UNKNOW_ERROR);
         return new ResponseEntity(response,HttpStatus.OK);
     }
