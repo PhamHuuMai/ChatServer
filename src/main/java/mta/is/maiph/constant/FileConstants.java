5b21cc227871232ed4fce9b7a0195257bc5b3b3c
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-10-21 12:46:27 +0700

    ssss

--- src/main/java/mta/is/maiph/constant/FileConstants.java
+++ src/main/java/mta/is/maiph/constant/FileConstants.java
@@ -9,9 +9,13 @@ public class FileConstants {
     public static final String HOST_FILE_ATT = "http://103.81.85.169/file";
 
     public static class FILE_TYPE {
-      public static final int OTHER = 3;
-      public static final int IMAGE = 4;
-      public static final int VIDEO = 5;
-      public static final int AUDIO = 6;
+
+        public static final int PING = 0;
+        public static final int TEXT = 1;
+        public static final int TYPING = 2;
+        public static final int OTHER = 3;
+        public static final int IMAGE = 4;
+        public static final int VIDEO = 5;
+        public static final int AUDIO = 6;
     }
 }
--- src/main/java/mta/is/maiph/websocket/handler/MessageReceiver.java
+++ src/main/java/mta/is/maiph/websocket/handler/MessageReceiver.java
@@ -1,10 +1,12 @@
 package mta.is.maiph.websocket.handler;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import mta.is.maiph.DAO.impl.ConversationDAO;
 import mta.is.maiph.DAO.impl.FileAttachmentDAO;
 import mta.is.maiph.DAO.impl.MessageDAO;
 import mta.is.maiph.DAO.impl.UnreadMsgDAO;
+import mta.is.maiph.constant.FileConstants;
 import mta.is.maiph.dto.connection.Message;
 import mta.is.maiph.session.SessionManager;
 import mta.is.maiph.websocket.entrypointchat.ReccieveMessageEntryPoint;
@@ -20,32 +22,33 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
  *
  * @author MaiPH
  */
+@Slf4j
 public class MessageReceiver extends TextWebSocketHandler {
-
+    
     static int i = 1;
     private static MessageDAO msgDAO = new MessageDAO();
     private static UnreadMsgDAO unreadMsgDAO = new UnreadMsgDAO();
     private static ConversationDAO cvsDAO = ConversationDAO.instance();
     private static FileAttachmentDAO fileAttDAO = new FileAttachmentDAO();
-
+    
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
         WebsocketSessionManager.remove(session.getId());
     }
-
+    
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
-
+        
     }
-
+    
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         super.handleTransportError(session, exception);
         WebsocketSessionManager.remove(session.getId());
     }
-
+    
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         try {
@@ -54,54 +57,68 @@ public class MessageReceiver extends TextWebSocketHandler {
             System.out.println(msg);
             JSONObject msgJson = (JSONObject) (new JSONParser()).parse(msg);
             Long msgType = (Long) msgJson.get("msg_type");
-            if (msgType == 0) {
-                String token = (String) msgJson.get("token");
-                String userId = SessionManager.instance().check(token);
-                WebsocketSessionManager.add(userId, session);
-            } else if (msgType == 1) {   // text msg
-                String userId = WebsocketSessionManager.getUserId(session.getId());
-                String toConversation = (String) msgJson.get("to");
-                String value = (String) msgJson.get("value");
-                Message msgDTO = new Message(userId, toConversation, msgType.intValue(), value);
-                ReccieveMessageEntryPoint.instance().add(msgDTO);
-                msgDAO.add(toConversation, userId, value, msgType.intValue());
-                //
-                cvsDAO.updateLastMsg(toConversation, value);
-                //
-                List<String> mems = cvsDAO.getListMem(toConversation);
-                mems.remove(userId);
-                for (String mem : mems) {
-                    unreadMsgDAO.incUnread(mem, toConversation);
+            final int msgTypeInt = Integer.valueOf(msgType.intValue());
+            log.info("============================================== msg type " + msgTypeInt);
+            switch (msgTypeInt) {
+                case FileConstants.FILE_TYPE.PING: {
+                    String token = (String) msgJson.get("token");
+                    String userId = SessionManager.instance().check(token);
+                    WebsocketSessionManager.add(userId, session);
+                    break;
                 }
-            } else if (msgType == 2) {  // typing
-                String userId = WebsocketSessionManager.getUserId(session.getId());
-                String toConversation = (String) msgJson.get("to");
-                Message msgDTO = new Message(userId, toConversation, msgType.intValue(), "");
-                ReccieveMessageEntryPoint.instance().add(msgDTO);
-            } else if (msgType == 3 || msgType == 4 || msgType == 5 || msgType == 6) {  // file attchment msg 
-                String userId = WebsocketSessionManager.getUserId(session.getId());
-                String toConversation = (String) msgJson.get("to");
-                String value = (String) msgJson.get("value");
-                String fileId = (String) msgJson.get("file_id");
-                Message msgDTO = new Message(userId, toConversation, msgType.intValue(), value);
-                ReccieveMessageEntryPoint.instance().add(msgDTO);
-                msgDAO.add(toConversation, userId, value, msgType.intValue());
-                //
-                cvsDAO.updateLastMsg(toConversation, value);
-                //
-                if (fileId != null) {
-                    fileAttDAO.updateFlag(fileId);
+                case FileConstants.FILE_TYPE.TEXT: {
+                    String userId = WebsocketSessionManager.getUserId(session.getId());
+                    String toConversation = (String) msgJson.get("to");
+                    String value = (String) msgJson.get("value");
+                    Message msgDTO = new Message(userId, toConversation, msgType.intValue(), value);
+                    ReccieveMessageEntryPoint.instance().add(msgDTO);
+                    msgDAO.add(toConversation, userId, value, msgType.intValue());
+                    //
+                    cvsDAO.updateLastMsg(toConversation, value);
+                    //
+                    List<String> mems = cvsDAO.getListMem(toConversation);
+                    mems.remove(userId);
+                    for (String mem : mems) {
+                        unreadMsgDAO.incUnread(mem, toConversation);
+                    }
+                    break;
                 }
-                List<String> mems = cvsDAO.getListMem(toConversation);
-                mems.remove(userId);
-                for (String mem : mems) {
-                    unreadMsgDAO.incUnread(mem, toConversation);
+                case FileConstants.FILE_TYPE.TYPING: {
+                    String userId = WebsocketSessionManager.getUserId(session.getId());
+                    String toConversation = (String) msgJson.get("to");
+                    Message msgDTO = new Message(userId, toConversation, msgType.intValue(), "");
+                    ReccieveMessageEntryPoint.instance().add(msgDTO);
+                    break;
                 }
+                case FileConstants.FILE_TYPE.OTHER:
+                    break;
+                case FileConstants.FILE_TYPE.IMAGE:
+                case FileConstants.FILE_TYPE.VIDEO:
+                case FileConstants.FILE_TYPE.AUDIO:
+                    String userId = WebsocketSessionManager.getUserId(session.getId());
+                    String toConversation = (String) msgJson.get("to");
+                    String value = (String) msgJson.get("value");
+                    String fileId = (String) msgJson.get("file_id");
+                    Message msgDTO = new Message(userId, toConversation, msgType.intValue(), value);
+                    ReccieveMessageEntryPoint.instance().add(msgDTO);
+                    msgDAO.add(toConversation, userId, value, msgType.intValue());
+                    //
+                    cvsDAO.updateLastMsg(toConversation, value);
+                    //
+                    if (fileId != null) {
+                        fileAttDAO.updateFlag(fileId);
+                    }
+                    List<String> mems = cvsDAO.getListMem(toConversation);
+                    mems.remove(userId);
+                    for (String mem : mems) {
+                        unreadMsgDAO.incUnread(mem, toConversation);
+                    }
+                    break;
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
+        
     }
-
+    
 }
