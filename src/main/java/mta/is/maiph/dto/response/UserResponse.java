45896d2cbdf1fc6d16c4719ea6c321cf511b32d8
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-11-21 23:46:17 +0700

    s

--- src/main/java/mta/is/maiph/dto/response/UserResponse.java
+++ src/main/java/mta/is/maiph/dto/response/UserResponse.java
@@ -32,10 +32,10 @@ public class UserResponse {
     }
 
     private String timeStringFormat(String lastLoginStr) {
-        lastLoginStr = lastLoginStr +"00000000000000000";
+        lastLoginStr = lastLoginStr + "00";
         String prefix = "Online ";
         long lastlogin = Util.format_yyyyMMddhhmmss(lastLoginStr);
-        long cur = System.currentTimeMillis();
-        return prefix + Util.getLastOnline(cur - Math.abs(lastlogin));
+        long cur = Util.format_yyyyMMddhhmmss(Util.format_yyyyMMddhhmmss(System.currentTimeMillis()));
+        return prefix + Util.getLastOnline(Math.abs(cur - lastlogin));
     }
 }
